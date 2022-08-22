8923f5c0e7ef43001ab969a73d36ad2203b96c2a
Mateusz <mat.bereda@gmail.com>
2022-08-22 12:58:07 +0200

    init

---
+++ src/main/java/pl/szkolenia/comarch/book/store/configuration/AppConfiguration.java
@@ -0,0 +1,9 @@
+package pl.szkolenia.comarch.book.store.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("pl.szkolenia.comarch.book.store")
+public class AppConfiguration {
+}
---
+++ src/main/java/pl/szkolenia/comarch/book/store/controllers/CommonController.java
@@ -0,0 +1,9 @@
+package pl.szkolenia.comarch.book.store.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CommonController {
+
+    
+}
